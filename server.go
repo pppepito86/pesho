d73e5365887d7f7a2f0a30b869fdcb977b18cba9
Petar Petrov <pppepito86@gmail.com>
2016-04-25 22:29:01 +0300

    download lom.zip

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import "net/http"
+import "os"
+import "path/filepath"
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -8,5 +10,13 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "http://robotikazabulgaria.com", http.StatusFound)
+	if r.URL.Path == "/lom.zip" {
+		w.Header().Set("Content-Disposition", "attachment; filename=lom.zip")
+		w.Header().Set("Content-Type", r.Header.Get("Content-Type"))
+		pwd, _ := os.Getwd()
+		file := pwd + string(filepath.Separator) + "lom.zip"
+		http.ServeFile(w, r, file)
+	} else {
+		http.Redirect(w, r, "http://robotikazabulgaria.com", http.StatusFound)
+	}
 }
